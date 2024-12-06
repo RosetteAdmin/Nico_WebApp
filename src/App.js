7a856490102bbd099c044c7ed9b84223cda74619
KajalRSLmit <kajal.suvarna@rosettesmartlife.com>
2024-12-06 14:14:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginScreen from "./Components/LoginScreen";
 import Header from "./Components/Header";
 import SideNavBar from "./Components/SideNavBar";
